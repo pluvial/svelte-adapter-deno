ff93710915e7b075af9df226967bcd1da094af1a
und3fined <cuonglee1011@gmail.com>
2021-08-20 18:02:29 +0700

    Update index.js
    initial app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { createServer } from './server.js';
-import { render } from './app.js';
+import { render, init } from './app.js';
 
 const hostname = Deno.env.get('HOST') ?? '0.0.0.0';
 const port = Deno.env.get('PORT') ?? 3000;
+
+init()
 
 const instance = createServer({ render }).listen({ hostname, port }, (err) => {
 	if (err) {
