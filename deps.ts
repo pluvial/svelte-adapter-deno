db071ba43bb53086b8abda1c70cb0eeb64de3ddb
jpaquim <joaopaquim@gmail.com>
2021-08-25 01:50:03 +0100

    Update dependencies to latest version

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { existsSync } from 'https://deno.land/std@0.95.0/fs/exists.ts';
-export { dirname, fromFileUrl, join } from 'https://deno.land/std@0.95.0/path/mod.ts';
-export { opine as app, serveStatic } from 'https://deno.land/x/opine@1.3.3/mod.ts';
+export { existsSync } from 'https://deno.land/std@0.106.0/fs/exists.ts';
+export { dirname, fromFileUrl, join } from 'https://deno.land/std@0.106.0/path/mod.ts';
+export { opine as app, serveStatic } from 'https://deno.land/x/opine@1.7.2/mod.ts';
