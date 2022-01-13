615e0960dd22369a1d070ddbfb6efe6274dfbae4
jpaquim <joaopaquim@gmail.com>
2022-01-13 23:27:02 +0000

    Migrate to oak

--- src/handler.js
+++ src/handler.js
@@ -1,79 +1,116 @@
-import { dirname, existsSync, fromFileUrl, join, serveStatic } from './deps.ts';
-import { getRawBody, headers_to_object } from './http.js';
+import { dirname, fromFileUrl, join, readAll, readerFromStreamReader } from './deps.ts';
+import { contentType } from './content-types.js';
 
 import { App } from 'APP';
-import { manifest } from 'MANIFEST';
+import { manifest, prerendered } from 'MANIFEST';
 
 const app = new App(manifest);
 
 const __dirname = dirname(fromFileUrl(import.meta.url));
 
-/**
- * @param {string} path
- * @param {number} max_age
- * @param {boolean} immutable
- */
-function serve(path, max_age, immutable = false) {
-	return existsSync(path)
-		? serveStatic(path, {
-				etag: true,
-				maxAge: max_age,
-				immutable
-				// gzip: true,
-				// brotli: true
-		  })
-		: // noop handler
-		  (_req, _res, next) => next();
-}
+const prefix = `/${manifest.appDir}/`;
 
-const ssr = async (req, res) => {
-	const url = new URL(req.url || '', 'http://localhost');
+export async function handler(ctx, next) {
+	// generated assets
+	if (ctx.request.url.pathname.startsWith(prefix)) {
+		return await ctx.send({
+			root: join(__dirname, 'client'),
+			headers: {
+				'cache-control': 'public, immutable, max-age=31536000',
+				// 'content-type': res.headers.get('content-type')
+				'content-type': contentType(ctx.request.url.pathname)
+			}
+		});
+	}
+
+	// prerendered pages and index.html files
+	const pathname = ctx.request.url.pathname.replace(/\/$/, '');
+	let file = pathname.substring(1);
+
+	try {
+		file = decodeURIComponent(file);
+	} catch (err) {
+		// ignore
+	}
+
+	if (manifest.assets.has(file)) {
+		return await ctx.send({ root: join(__dirname, 'static') });
+	}
+	file += '/index.html';
+	if (manifest.assets.has(file)) {
+		return await ctx.send({ path: file, root: join(__dirname, 'static') });
+	}
+	if (prerendered.has(pathname || '/')) {
+		return await ctx.send({ path: file, root: join(__dirname, 'prerendered') });
+	}
+
+	// dynamically-generated pages
+	const req = ctx.request.originalRequest;
 
 	let body;
-
 	try {
 		body = await getRawBody(req);
 	} catch (err) {
-		res.statusCode = err.status || 400;
-		return res.end(err.reason || 'Invalid request body');
+		console.error(err);
+		ctx.response.status = err.status || 400;
+		ctx.response.body = err.reason || 'Invalid request body';
+		return await next();
 	}
 
 	const rendered = await app.render({
 		method: req.method,
 		headers: headers_to_object(req.headers), // TODO: what about repeated headers, i.e. string[]
-		url,
+		url: req.url,
 		rawBody: body
 	});
 
 	if (rendered) {
-		res.setStatus(rendered.status);
-		res.set(rendered.headers);
-		res.send(rendered.body);
+		ctx.response.status = rendered.status;
+		ctx.response.headers = make_headers(rendered.headers);
+		ctx.response.body = rendered.body;
 	} else {
-		res.setStatus(404);
-		res.send('Not found');
+		ctx.response.status = 404;
+		ctx.response.body = 'Not found';
 	}
-};
-
-function sequence(handlers) {
-	return (req, res, next) => {
-		/** @param {number} i */
-		function handle(i) {
-			handlers[i](req, res, () => {
-				if (i < handlers.length) handle(i + 1);
-				else next();
-			});
-		}
-
-		handle(0);
-	};
 }
 
-export const handler = sequence(
-	[
-		serve(join(__dirname, '/client'), 31536000, true),
-		serve(join(__dirname, '/static'), 0),
-		serve(join(__dirname, '/prerendered'), 0),
-		ssr
-	].filter(Boolean)
-);
+/**
+ * Converts request headers from Headers to a plain key-value object, as used in node
+ * @param {Headers} headers Browser/Deno Headers object
+ * @returns {object} Plain key-value headers object
+ */
+const headers_to_object = (headers) => Object.fromEntries(headers.entries());
+
+/** @param {Record<string, string | string[]>} headers */
+function make_headers(headers) {
+	const result = new Headers();
+	for (const header in headers) {
+		const value = headers[header];
+		if (typeof value === 'string') {
+			result.set(header, value);
+			continue;
+		}
+		for (const sub of value) {
+			result.append(header, sub);
+		}
+	}
+	return result;
+}
+
+/**
+ * @param {Request} req Deno server request object
+ * @returns {Promise<null | Uint8Array>} Resolves with the request body raw buffer
+ */
+async function getRawBody(req) {
+	const { body, headers } = req;
+	// console.log({ body, headers });
+	// take the first content-type header
+	// TODO: is split(',') enough?
+	const type = headers.get('content-type')?.split(/,;\s*/)?.[0];
+	if (type === null || body === null) {
+		return null;
+	}
+
+	const data = await readAll(readerFromStreamReader(req.body.getReader()));
+	return data;
+}
