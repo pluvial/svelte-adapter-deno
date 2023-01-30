1e967a25b6cd71f451401dfecdb7a2e31a5b9a33
jpaquim <joaopaquim@gmail.com>
2023-01-30 16:05:24 +0000

    Update example to latest template

--- examples/svelte-kit-demo/src/app.d.ts
+++ examples/svelte-kit-demo/src/app.d.ts
@@ -1,4 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {}
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+export {};
